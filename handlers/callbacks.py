62636e9bde75249cba830742b44b1b83e9f6438a
Gavrya69 <andreygav77@gmail.com>
2026-01-01 03:52:31 +0300

    import F

--- handlers/callbacks.py
+++ handlers/callbacks.py
@@ -1,18 +1,17 @@
-from aiogram import Router
+from aiogram import Router, F
 from aiogram.types import CallbackQuery
 from services.downloader import download_youtube
 
 router = Router()
 
-@router.callback_query(lambda c: c.data == 'cancel')
+@router.callback_query(F.data == 'cancel')
 async def cancel_download(callback: CallbackQuery):
     await callback.message.edit_text('похуй, не хочу скачивать')
     await callback.answer()
 
 
-@router.callback_query(lambda c: c.data.startswith('download:'))
+@router.callback_query(F.data.startswith('download:'))
 async def start_download(callback: CallbackQuery):
     await callback.answer('Ща скачаю погодь...')
 
     url = callback.data.split("download:", 1)[1]
-
